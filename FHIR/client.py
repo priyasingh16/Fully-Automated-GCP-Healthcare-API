7479244c2739c2d568d44de86e5fc0bf5bef2a9c
parth <parthtandel0594@gmail.com>
2019-11-04 13:07:27 -0500

    client cleanup

--- FHIR/client.py
+++ FHIR/client.py
@@ -1,14 +1,44 @@
 from google.cloud import bigquery
 
 class client:
+    """
+    Description
+    -----------
+    The class deals with client connection to google big query resource to get record form mimic table.
+    """
+
     def __init__(self):
+        """
+        Description
+        -----------
+        Create connection to bigquery using GOOGLE_APPLICATION_CREDENTIALS 
+        """
         self.client = bigquery.Client()
 
     def closeConnection(self):
+        """
+        Description
+        -----------
+        Close connection to big query server.
+        """
+
         if self.client:
             self.client.close()
 
     def queryRecords(self, query):
+        """
+        Description
+        -----------
+        Query big query database which contains mimic records
+        ----------
+        query : String
+            Valid sql like query to query record from the mimic tables.
+        Returns
+        ----------
+        query_job : Record:
+            list of dictionary like record for they query or false if client not initialized
+        """
+
         if self.client:
             query_job = self.client.query(query)
             return query_job.result()
--- FHIR/storageClient.py
+++ FHIR/storageClient.py
@@ -1,18 +1,44 @@
 from google.cloud import firestore
-
-
 class DatastoreClient:
     def __init__(self):
+        """
+        Description
+        -----------
+        Create connection to firestore server using GOOGLE_APPLICATION_CREDENTIALS 
+        """
+
         self.db = firestore.Client()
 
     def closeConnection(self):
+        """
+        Description
+        -----------
+        Close connection to fire store server.
+        """
+
         if self.db:
             self.db.close()
 
     def insertData(self, kind, name, data):
-        print(name)
+        """
+        Description
+        -----------
+        Inserts data into firestore NOSQL db 
+        ----------
+        Kind : String
+            name of the collection to store data into.
+        name : String
+            unique identifier for that document.
+        data : dict
+            document in dictionary key value pair format.
+        Returns
+        ----------
+        _ : Boolean
+            if success true else false
+        """
         if self.db:
             doc_ref = self.db.collection(kind).document(name)
             doc_ref.set(data)
+            return True
         else:
             return False    
