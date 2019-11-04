4d486fd307ca906f3c7c5c47d03547aec6364869
parth <parthtandel0594@gmail.com>
2019-11-04 12:53:05 -0500

    medication cleanup

--- FHIR/medication.py
+++ FHIR/medication.py
@@ -3,25 +3,41 @@ from pprint import pprint
 
 
 class Medication:
-    # Procedure start and stop times recorded for MetaVision patients.
+    """
+    Description
+    -----------
+    Get relevent information about a paticular medicine
+    """
+
     def __init__(self):
+        """
+        Description
+        -----------
+        Initializes google cloud big query client to query FHIR data.
+        """
         self.cl = client()
 
-    def all_patient(self):
-        query_string = """SELECT SUBJECT_ID from `green-gasket-256323.mimiciii_fullyautomated.PATIENTS`;"""
-        results = self.cl.queryRecords(query_string)
-        SUBJECT_IDS = []
-        for row in results:
-            SUBJECT_IDS.append(row["SUBJECT_ID"])
-        return SUBJECT_IDS
-
     def get_medication(self, id):
+        """
+        Description
+        -----------
+        Extracts Information about all medication in the mimic catalog
+        from prescription table.
+        Parameters
+        ----------
+        id : Integer
+            Subject id of the patient to fetch prescription record.
+        Returns
+        ----------
+        medicine_res : list
+            list of dicts which contains information about a particular drug
+        """
+
         query_string = """SELECT DRUG_NAME_GENERIC, FORMULARY_DRUG_CD, NDC, PROD_STRENGTH, 
         DOSE_VAL_RX, FORM_UNIT_DISP FROM `green-gasket-256323.mimiciii_fullyautomated.PRESCRIPTIONS`
         where SUBJECT_ID = {};"""
 
         query_string = query_string.format(id)
-        print(query_string)
         results = self.cl.queryRecords(query_string)
 
         r = []
@@ -37,31 +53,24 @@ class Medication:
             r.append(res)
 
 
-        medication_res = []
+        medicine_res = []
         for res in r:
-            p_json = {
-          "resourceType" : "Medication",
-          "identifier" : res['NDC'],
-          "code" : res['FORMULARY_DRUG_CD'],
-          "status" : None,
-          "manufacturer" : None,
-          "form" : res['FORM_UNIT_DISP'],
-          "amount" : res['DOSE_VAL_RX'],
-          "ingredient" : [{
-            "itemCodeableConcept" : res['FORMULARY_DRUG_CD'],
-            "itemReference" : res['DRUG_NAME_GENERIC'],
-            "isActive" : None,
-            "strength" : res['PROD_STRENGTH']
-          }],
-          "batch" : None
-        }
+            medicine_info = {
+                "resourceType" : "Medication",
+                "identifier" : res['NDC'],
+                "code" : res['FORMULARY_DRUG_CD'],
+                "status" : None,
+                "manufacturer" : None,
+                "form" : res['FORM_UNIT_DISP'],
+                "amount" : res['DOSE_VAL_RX'],
+                "ingredient" : [{
+                    "itemCodeableConcept" : res['FORMULARY_DRUG_CD'],
+                    "itemReference" : res['DRUG_NAME_GENERIC'],
+                    "isActive" : None,
+                    "strength" : res['PROD_STRENGTH']
+                }],
+                "batch" : None
+            }
 
-            medication_res.append(p_json)
-        return medication_res
-
-
-if __name__ == "__main__":
-#
-    p = Medication()
-    for id in p.all_patient():
-        pprint(p.get_medication(id))
+            medicine_res.append(medicine_info)
+        return medicine_res
