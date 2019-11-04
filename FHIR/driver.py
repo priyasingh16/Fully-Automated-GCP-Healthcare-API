975b43bb0ae65f250e72546fda6a44dd845fec9e
parth <parthtandel0594@gmail.com>
2019-11-04 13:25:42 -0500

    driver test

--- FHIR/driver.py
+++ FHIR/driver.py
@@ -1,15 +1,40 @@
 import logging
+import time
 from storageClient import DatastoreClient
-from patient import patient
-
+from patient import Patient
+from encounter import Encounter
+from medication import Medication
+from medication_dispense import MedicationDispense
+from report import DiagnosticReport
+from procedure import Procedure
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='logs/app.log',level=logging.INFO)
 
 if __name__ == "__main__":
-    p = patient()
     d = DatastoreClient()
+    
+    p = Patient()
+    e = Encounter()
+    m = Medication()
+    dr = DiagnosticReport()
+    md = MedicationDispense()
+    pr = Procedure()
+
     all_patient = sorted(p.all_patient())
     for id in all_patient:
+        start = time.time()
+        logger.info('Extracting Data For Id: ' + str(id) + ', time : ' + str(start))
         print(p.get_patient(id))
+        print(e.get_encounter(id))
+        print(m.get_medication(id))
+        print(md.get_medication_dispense(id))
+        print(dr.get_diagnostic_report(id))
+        print(pr.get_procedure(id))
+
+
+        end = time.time()
+
+        logger.info('Extracting Data For Id: ' + str(id) + ', time : ' + str(end))
+        logger.info('Rime taken for Id: ' + str(id) + ', time : ' + str(end-start))
+
         break
-        # logger.info('Extracting Data For Id: ' + str(id))
