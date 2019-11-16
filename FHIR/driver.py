b2544b83fe72ffbfe40ad4cf56d79bd442ef00f7
parth <parthtandel0594@gmail.com>
2019-11-15 19:14:45 -0500

    data deployment process

--- FHIR/driver.py
+++ FHIR/driver.py
@@ -8,6 +8,7 @@ from medication_dispense import MedicationDispense
 from report import DiagnosticReport
 from procedure import Procedure
 from observations import Observations
+import json
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='logs/app.log',level=logging.INFO)
 
@@ -26,16 +27,24 @@ if __name__ == "__main__":
     for id in all_patient:
         start = time.time()
         logger.info('Extracting Data For Id: ' + str(id) + ', time : ' + str(start))
-        print(p.get_patient(id))
-        print(e.get_encounter(id))
-        print(m.get_medication(id))
-        print(o.get_observations(id))
-        print(md.get_medication_dispense(id))
-        print(dr.get_diagnostic_report(id))
-        print(pr.get_procedure(id))
+        patient_info = p.get_patient(id)
+        encounter_info = e.get_encounter(id)
+        medicine_info = m.get_medication(id)
+        observation_info = o.get_observations(id)
+        medication_info = md.get_medication_dispense(id)
+        diagnostic_info = dr.get_diagnostic_report(id)
+        procedure_info = pr.get_procedure(id)
+
+        d.insertData("patient", str(id), patient_info)
+        d.insertData("encounter", str(id), {"data" : encounter_info})
+        d.insertData("medicine", str(id), {"data" : medicine_info})
+        d.insertData("obsevation", str(id), {"data" : observation_info})
+        d.insertData("medication", str(id), {"data" : medication_info})
+        d.insertData("diagnostics", str(id), {"data" : diagnostic_info})
+        d.insertData("procedure", str(id), {"data" : procedure_info})
+
         end = time.time()
 
         logger.info('Extracting Data For Id: ' + str(id) + ', time : ' + str(end))
         logger.info('Rime taken for Id: ' + str(id) + ', time : ' + str(end-start))
 
-        break
