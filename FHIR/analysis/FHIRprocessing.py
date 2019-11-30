c556286497085df73c986a970ccc00fc9ac20b86
Monica Mishra <mishra.m@husky.neu.edu>
2019-11-29 23:17:54 -0500

    patient info added

--- FHIR/analysis/FHIRprocessing.py
+++ FHIR/analysis/FHIRprocessing.py
@@ -165,28 +165,29 @@ class FHIRprocessor:
 
     @staticmethod
     def observation_processor(paths):
-    observation_info_output = {}
-    for path in paths:
-        with open(path, 'r') as fl:
-            js_data = ast.literal_eval(fl.read())
-            js_data = ast.literal_eval(js_data['data'].replace("'" , "\""))
-            for data in js_data:
-                subject_id = data["subject"]
-                hospital_id = data["encounter"]
-                if subject_id not in observation_info:
-                    observation_info[subject_id] = {}
-                    observation_info_output[subject_id] = {}
-                if hospital_id not in observation_info[subject_id]:
-                    observation_info[subject_id][hospital_id] = {"abnormal_test_count" : 0, "all_test_count":0}
-                observation_info[subject_id][hospital_id]["abnormal_test_count"] += data["valueInteger"]
-                observation_info[subject_id][hospital_id]["all_test_count"] += data["valueQuantity"]
-            
-            abnormal_tc = observation_info[subject_id][hospital_id]["abnormal_test_count"] 
-            total_tc = observation_info[subject_id][hospital_id]["all_test_count"] 
+        observation_info_output = {}
+        observation_info = {}        
+        for path in paths:
+            with open(path, 'r') as fl:
+                js_data = ast.literal_eval(fl.read())
+                js_data = ast.literal_eval(js_data['data'].replace("'" , "\""))
+                for data in js_data:
+                    subject_id = data["subject"]
+                    hospital_id = data["encounter"]
+                    if subject_id not in observation_info:
+                        observation_info[subject_id] = {}
+                        observation_info_output[subject_id] = {}
+                    if hospital_id not in observation_info[subject_id]:
+                        observation_info[subject_id][hospital_id] = {"abnormal_test_count" : 0, "all_test_count":0}
+                    observation_info[subject_id][hospital_id]["abnormal_test_count"] += data["valueInteger"]
+                    observation_info[subject_id][hospital_id]["all_test_count"] += data["valueQuantity"]
+                
+                abnormal_tc = observation_info[subject_id][hospital_id]["abnormal_test_count"] 
+                total_tc = observation_info[subject_id][hospital_id]["all_test_count"] 
 
-            if hospital_id not in observation_info_output:
-                observation_info_output[subject_id][hospital_id] = np.array([abnormal_tc, total_tc])
-    return observation_info_output
+                if hospital_id not in observation_info_output:
+                    observation_info_output[subject_id][hospital_id] = np.array([abnormal_tc, total_tc])
+        return observation_info_output
 
     @staticmethod
     def procedure_processor(paths):
@@ -268,15 +269,15 @@ class FHIRprocessor:
                     if hospital_id not in encounter_info[subject_id]:
                         encounter_info[subject_id][hospital_id] = {}
                     encounter_info[subject_id][hospital_id]["length"] = length
-                    encounter_info[subject_id][hospital_id]["reason_code"] = data["reasonCode"]
+                    # encounter_info[subject_id][hospital_id]["reason_code"] = data["reasonCode"]
                     code_list.append([data["reasonCode"]])
                     diag = " ".join([i["condition"] for i in ast.literal_eval(data["diagnosis"]) if i["condition"] != None]).lower()
                     encounter_info[subject_id][hospital_id]["encounter_diagnosis"] = diag
                     words = [w for w in diag.split() if not w in stop_words]
                     text.append(" ".join(words))
             
-        enc = OneHotEncoder(handle_unknown='ignore')
-        categorical_diagnosis = enc.fit_transform(code_list)
+        # enc = OneHotEncoder(handle_unknown='ignore')
+        # categorical_diagnosis = enc.fit_transform(code_list)
 
         vectorizer = TfidfVectorizer(max_df=0.95, min_df=0.05)
         vector = vectorizer.fit_transform(text)
@@ -301,7 +302,7 @@ class FHIRprocessor:
                 model_inp = vectorizer.transform([" ".join(words)])
                 encoded_output = encoder.predict(model_inp)
                 one_observation = [encounter_info[subject_id][hospital_id]["length"]]
-                one_observation.extend(categorical_diagnosis[ind].A[0])
+                # one_observation.extend(categorical_diagnosis[ind].A[0])
                 one_observation.extend(encoded_output[0])
                 ind += 1
                 if subject_id not in encounter_auto_encoded:
@@ -317,11 +318,12 @@ class FHIRprocessor:
         for path in paths:
             with open(path, 'r') as fl:
                 js_data = ast.literal_eval(fl.read())
-                for data in js_data["data"]:
-                    subject_id = data["identifier"]
-                    if subject_id not in patient_info:
-                        patient_info[subject_id] = {}
-                    patient_info['gender'] = data["gender"]
-                    patient_info['survived'] = data["deceasedBoolean"]
-                    
+                subject_id = js_data["identifier"]
+                if subject_id not in patient_info:
+                    patient_info[subject_id] = {}
+                patient_info[subject_id]['gender'] = js_data["gender"]
+                if js_data["deceasedBoolean"] == '1':
+                    patient_info[subject_id]['survived'] = False
+                else:
+                    patient_info[subject_id]['survived'] = True
         return patient_info
